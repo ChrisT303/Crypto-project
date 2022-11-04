b810db6fd1a93563b45ac92d0f01924952750736
deftonechris <deftonechris@msn.com>
2022-11-04 17:12:01 -0600

    specefic news rendering

--- src/components/Currencies.jsx
+++ src/components/Currencies.jsx
@@ -30,12 +30,13 @@ if(isFetching) return 'Loading...';
       )}
     <Row gutter={[32, 32]} className="crypto-card-container">
        {cryptos?.map((currency) => (
-          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
             <Link to={`/crypto/${currency.id}`}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
                 extra={<img className="crypto-image" alt='#' src={currency.iconUrl} />}
                 hoverable
+                
               >
                 <p>Price: {millify(currency.price)}</p>
                 <p>Market Cap: {millify(currency.marketCap)}</p>
